8717288697db8e523f9126bfd2035289f5669059
Fabio Garcia <fabiorgarcia@gmail.com>
2024-07-02 09:45:04 -0300

    Search

--- react-test/src/main.tsx
+++ react-test/src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider  } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
+import Home from './routes/Home.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/react-test",
+    element: <App />,
+    children: [
+      {
+        path: "/react-test",
+        element: <Home />,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
